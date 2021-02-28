bc70aca3d609ffc14ee03413999f3db10237edc7
Tiago Pereira <tiagopatureba@hotmail.com>
2021-02-28 19:48:09 -0300

    Modelagem de tabuleiro - Inicial

--- Tabuleiro.php
+++ Tabuleiro.php
@@ -4,14 +4,51 @@ class Tabuleiro {
     private $tabuleiro = array([7], [7]);
 
     public function __construct(){
-        
+
     }
 
-    public function getTabuleiro(){
-        return $this->tabuleiro;
+    public function getTabuleiroPecaCasa($row, $col){
+        if(!empty($this->tabuleiro[$row][$col])){
+            echo $this->tabuleiro[$row][$col]->getImagem();
+        }
+        return null;
     }
 
-    public function setTabuleiro($tabuleiro){
-        $this->tabuleiro[1][0] = new Peao();
+    public function setTabuleiroInicial(){
+        $this->tabuleiro[0][0] = new Torre("TorrePreta");
+        $this->tabuleiro[0][1] = new Cavalo("CavaloPreto");
+        $this->tabuleiro[0][2] = new Bispo("BispoPreto");
+        $this->tabuleiro[0][3] = new Rainha("RainhaPreta");
+        $this->tabuleiro[0][4] = new Rei("ReiPreto");
+        $this->tabuleiro[0][5] = new Bispo("BispoPreto");
+        $this->tabuleiro[0][6] = new Cavalo("CavaloPreto");
+        $this->tabuleiro[0][7] = new Torre("TorrePreta");
+        $this->tabuleiro[1][0] = new Peao("PeaoPreto");
+        $this->tabuleiro[1][1] = new Peao("PeaoPreto");
+        $this->tabuleiro[1][2] = new Peao("PeaoPreto");
+        $this->tabuleiro[1][3] = new Peao("PeaoPreto");
+        $this->tabuleiro[1][4] = new Peao("PeaoPreto");
+        $this->tabuleiro[1][5] = new Peao("PeaoPreto");
+        $this->tabuleiro[1][6] = new Peao("PeaoPreto");
+        $this->tabuleiro[1][7] = new Peao("PeaoPreto");
+
+        $this->tabuleiro[6][0] = new Peao("PeaoBranco");
+        $this->tabuleiro[6][1] = new Peao("PeaoBranco");
+        $this->tabuleiro[6][2] = new Peao("PeaoBranco");
+        $this->tabuleiro[6][3] = new Peao("PeaoBranco");
+        $this->tabuleiro[6][4] = new Peao("PeaoBranco");
+        $this->tabuleiro[6][5] = new Peao("PeaoBranco");
+        $this->tabuleiro[6][6] = new Peao("PeaoBranco");
+        $this->tabuleiro[6][7] = new Peao("PeaoBranco");
+        $this->tabuleiro[7][0] = new Torre("TorreBranca");
+        $this->tabuleiro[7][1] = new Cavalo("CavaloBranco");
+        $this->tabuleiro[7][2] = new Bispo("BispoBranco");
+        $this->tabuleiro[7][3] = new Rainha("RainhaBranca");
+        $this->tabuleiro[7][4] = new Rei("ReiBranco");
+        $this->tabuleiro[7][5] = new Bispo("BispoBranco");
+        $this->tabuleiro[7][6] = new Cavalo("CavaloBranco");
+        $this->tabuleiro[7][7] = new Torre("TorreBranca");
+
     }
+
 }
--- index.php
+++ index.php
@@ -4,50 +4,26 @@ spl_autoload_register(function ($class_name){
 });
 
 
-echo "<div align='center'> Projeto Xadrez - BD</div>";
-echo '<br><br>';
+echo "<div align='center'> <h2>Projeto Xadrez PHP</h2>
+    <h3>Reprodutor de Jogadas a partir de Instruções de Notação Algébrica</h3>
+    <h6>Previsão de Término em 30/01/2021</h6></div>";
+echo '<br>';
 
-$peaoPreto = new Peao('PeaoPreto');
-$torrePreta = new Torre('TorrePreta');
-$cavaloPreto = new Cavalo('CavaloPreto');
-$bispoPreto = new Bispo('BispoPreto');
-$rainhaPreta = new Rainha('RainhaPreta');
-$reiPreto = new Rei('ReiPreto');
-
-$peaoBranco = new Peao('PeaoBranco');
-$torreBranca = new Torre('TorreBranca');
-$cavaloBranco = new Cavalo('CavaloBranco');
-$bispoBranco = new Bispo('BispoBranco');
-$rainhaBranca = new Rainha('RainhaBranca');
-$reiBranco = new Rei('ReiBranco');
-
-echo $peaoPreto->getImagem();
-echo $torrePreta->getImagem();
-echo $cavaloPreto->getImagem();
-echo $bispoPreto->getImagem();
-echo $rainhaPreta->getImagem();
-echo $reiPreto->getImagem();
-echo $peaoBranco->getImagem();
-echo $torreBranca->getImagem();
-echo $cavaloBranco->getImagem();
-echo $bispoBranco->getImagem();
-echo $rainhaBranca->getImagem();
-echo $reiBranco->getImagem();
-
-$tabuleiro[0][0] = $torrePreta;
+$tabuleiro = new Tabuleiro();
+$tabuleiro->setTabuleiroInicial();
 
 
-echo '<table width="45%" height="80%" border="1" align="center">';
+echo '<table border="1" align="center">';
 for ($i = 0; $i <= 7; $i++) {
     echo '<tr>';
     for($j = 0; $j<= 7; $j++){
         if ( (($i % 2) != 0) and (($j % 2) == 0) or (($i % 2) == 0) and (($j % 2) != 0)){
-            echo '<td align="center" bgcolor="#C3C3C3">';
-
+            echo '<td align="center" bgcolor="#C3C3C3" width="90px" height="90px">';
+            $tabuleiro->getTabuleiroPecaCasa($i, $j);
             echo '</td>';
         } else {
-            echo '<td align="center" bgcolor="#FFFFF">';
-                    echo $tabuleiro[0][0]->getImagem();
+            echo '<td align="center" bgcolor="#FFFFF" width="90px" height="90px">';
+            $tabuleiro->getTabuleiroPecaCasa($i, $j);
             echo '</td>';
         }
     }
@@ -56,17 +32,13 @@ for ($i = 0; $i <= 7; $i++) {
     echo '</tr>';
 }
 echo '<tr border="0" align="center">';
-    echo '<td border="0">A</td>';
-    echo '<td border="0">B</td>';
-    echo '<td border="0">C</td>';
-    echo '<td border="0">D</td>';
-    echo '<td border="0">E</td>';
-    echo '<td border="0">F</td>';
-    echo '<td border="0">G</td>';
-    echo '<td border="0">H</td>';
+echo '<td border="0">A</td>';
+echo '<td border="0">B</td>';
+echo '<td border="0">C</td>';
+echo '<td border="0">D</td>';
+echo '<td border="0">E</td>';
+echo '<td border="0">F</td>';
+echo '<td border="0">G</td>';
+echo '<td border="0">H</td>';
 echo '</tr>';
 echo '</table>';
-
-
-
-
